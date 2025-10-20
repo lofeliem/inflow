c6b23a66be753365108fef1bb7b8eb839d68e578
bd <920130017@qq.com>
2025-10-20 20:50:28 +0800

    feat: update code

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Welcome to your Expo app 👋
+回到bdzhang目录 sudo ~/mongodb/mongodb/bin/mongod --dbpath ~/github/inflow/database
 
 This is an [Expo](https://expo.dev) project created with [`create-expo-app`](https://www.npmjs.com/package/create-expo-app).
 
--- components/smartimage/index.jsx
+++ components/smartimage/index.jsx
@@ -4,7 +4,6 @@ import { StyleSheet, View } from 'react-native'
 
 export default function SmartImage({ imgUrl, showBlurBg }) {
   const [imgLoading, setImgLoading] = useState(false)
-  console.log('iiii', imgUrl)
 
   const onLoadStart = () => {
     console.log('onLoadStart')
